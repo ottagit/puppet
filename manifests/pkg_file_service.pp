e2fba02b635e1fc4ff150632c3cf1dacd4380184
ottagit <ottaosawa@gmail.com>
2022-05-19 09:21:54 +0000

    order resources using require
    Explicit ordering is hardly used nowadays. In
    legacy code, require was used as ordering of
    resources was mostly arbitrary.

--- manifests/pkg_file_service.pp
+++ manifests/pkg_file_service.pp
@@ -1,18 +1,20 @@
 package {
-  "mysql":
+  "mysql-server":
     ensure => 'installed',
     notify => Service['mysql'],
 }
 
 file {
   "/etc/mysql/mysql.cnf":
-    ensure => 'file',
-    source => '/examples/files/mysql.cnf',
-    notify => Service['mysql'],
+    ensure  => 'file',
+    source  => '/examples/files/mysql.cnf',
+    notify  => Service['mysql'],
+    require => Package['mysql-server'],
 }
 
 service {
   "mysql":
-    ensure   => 'running',
-    enable   => 'true',
+    ensure  => 'running',
+    enable  => 'true',
+    require => [Package['mysql-server'], File['/etc/mysql/mysql.cnf/']],
 }
