cf49595031148eb21a2576471c4a7dd7d44505cb
ottagit <ottaosawa@gmail.com>
2022-05-20 03:13:56 +0000

    document purge behaviour on package

--- manifests/package_remove.pp
+++ manifests/package_remove.pp
@@ -1,4 +1,8 @@
 package {
   "apparmor":
-    ensure   => 'absent',
+    ensure  => 'absent',
+    # By default, when Puppet removes packages, it leaves in
+    # place files managed by the package. The following line
+    # purges all files associated with the package.
+    #ensure => 'purged'
 }
