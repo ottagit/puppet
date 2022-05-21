f96b6dab5e443c9d9b9e259fa6fee1970436575a
ottagit <ottaosawa@gmail.com>
2022-05-21 20:37:31 +0000

    restart service on change notification

--- manifests/service_hasstatus.pp
+++ manifests/service_hasstatus.pp
@@ -1,7 +1,9 @@
 service {
   "ntp":
-    ensure    => 'running',
-    enable    => 'true',
-    hasstatus => 'false',
-    pattern   => 'ntpd',
+    ensure     => 'running',
+    enable     => 'true',
+    hasstatus  => 'false',
+    pattern    => 'ntpd',
+    hasrestart => 'true',
+    restart    => '/bin/echo Restarting >>/tmp/debug.log && systemctl restart ntp',
 }
