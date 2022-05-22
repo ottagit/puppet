c9f7d0d8869167ea469f7372c24e3d20f25b3f0c
ottagit <ottaosawa@gmail.com>
2022-05-22 18:56:40 +0000

    run exec resource unless command succeeds

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -3,4 +3,5 @@ exec {
     command => '/bin/echo Hello, this is `whomai` >/tmp/hello-ubuntu.txt',
     user    => 'ubuntu',
     creates => '/tmp/hello-ubuntu.txt',
+    #unless  => '/bin/ls /tmp/ | grep hello-ubuntu',
 }
