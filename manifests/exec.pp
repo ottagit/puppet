e27822da900fc07597d71936fb90cd3d2a851ef1
ottagit <ottaosawa@gmail.com>
2022-05-22 19:25:39 +0000

    set log output and timeout for exec

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -1,7 +1,9 @@
 exec {
   "say-hello":
-    command => '/bin/echo Hello, this is `whomai` >/tmp/hello-ubuntu.txt',
-    user    => 'batoto',
-    creates => '/tmp/hello-ubuntu.txt',
-    #unless  => '/bin/ls /tmp/ | grep hello-ubuntu',
+    command   => '/bin/echo Hello, this is `whomai` >/tmp/hello-ubuntu.txt',
+    user      => 'batoto',
+    creates   => '/tmp/hello-ubuntu.txt',
+    logoutput => 'true',
+    timeout   => '60',
+    #unless   => '/bin/ls /tmp/ | grep hello-ubuntu',
 }
