77215a1b3396bcb8c88e8cf4a3f445723facb704
ottagit <ottaosawa@gmail.com>
2022-06-02 11:23:55 +0000

    remove duplicate resource decalration

--- manifests/hiera_users.pp
+++ manifests/hiera_users.pp
@@ -1,9 +1,3 @@
-lookup('users', Array[String]).each | String $username | {
-  user{"$username":
-    ensure => 'present',
-  }
-}
-
 lookup('users2', Hash, 'hash').each | String $username, Hash $attrs | {
   user{"$username":
     *      => $attrs,
